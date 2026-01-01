95a44e2a175837da633343e2eabaad03028f443d
Viktor Horvath <ric.flair.wcw@gmail.com>
2026-01-01 11:03:49 +0000

    Added lombok to App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,9 @@
 import org.springframework.stereotype.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.acme.converter.SzuppeKonverter;
+import org.lombok.Getter;
 
+@Getter
 public class App {
 	
 	public static void main(String[] args) {
